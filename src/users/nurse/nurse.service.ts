f3fcd36ad4f861cb5769588bcdc8b1738d021c45
João Victor Garcia <jvictor_sk8@hotmail.com>
2025-05-05 20:31:30 -0300

    add nurse service

--- src/users/nurse/nurse.service.ts
+++ src/users/nurse/nurse.service.ts
@@ -3,32 +3,64 @@ import { CreateNurseDto } from './dto/create-nurse.dto';
 import { UpdateNurseDto } from './dto/update-nurse.dto';
 import { Nurse } from './entities/nurse.entity';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class NurseService {
   constructor(private readonly prisma: PrismaService) { }
 
-  create(createNurseDto: CreateNurseDto, adminId: number): Promise<Nurse> {
-    throw new Error('Method not implemented.');
+  async create(createNurseDto: CreateNurseDto, adminId: number): Promise<Nurse> {
+    const data = {
+      ...createNurseDto,
+      password: await bcrypt.hash(createNurseDto.password, 10),
+      adminId: adminId,
+    };
+
+    return await this.prisma.nurse.create({ data });
   }
 
-  findAll(): Promise<Nurse[]> {
-    throw new Error('Method not implemented.');
+  async findAll(): Promise<Nurse[]> {
+    return await this.prisma.nurse.findMany();
   }
 
-  findOne(id: number): Promise<Nurse | null> {
-    throw new Error('Method not implemented.');
+  async findOne(id: number): Promise<Nurse | null> {
+    const nurse = await this.prisma.nurse.findUnique({ where: { id } });
+
+    if (!nurse) {
+      return Promise.resolve(null);
+    }
+
+    return nurse;
   }
 
-  findByEmail(email: string): Promise<Nurse | null> {
-    throw new Error('Method not implemented.');
+  async findByEmail(email: string): Promise<Nurse | null> {
+    const nurse = await this.prisma.nurse.findUnique({ where: { email } });
+
+    if (!nurse) {
+      return Promise.resolve(null);
+    }
+
+    return nurse;
   }
 
-  update(id: number, updateNurseDto: UpdateNurseDto): Promise<Nurse> {
-    throw new Error('Method not implemented.');
+  async update(id: number, updateNurseDto: UpdateNurseDto): Promise<Nurse> {
+    return await this.prisma.nurse.update({
+      where: { id },
+      data: updateNurseDto,
+    });
   }
 
-  remove(id: number): Promise<Nurse> {
-    throw new Error('Method not implemented.');
+  async remove(id: number): Promise<Nurse> {
+    const user = await this.findOne(id);
+
+    const data = {
+      ...user,
+      isActive: false,
+    };
+
+    return this.prisma.nurse.update({
+      where: { id },
+      data: data,
+    });
   }
 }
